d45b692b76cf88b266e9446bb3a79f82efe07ade
mateusz socko <dataochi@gmail.com>
2021-04-19 12:00:17 +0200

    poprawka2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let alpha = "1" ;
-let beta = "2" ;
-let gama = "3" ;
+let alpha = 1 ;
+let beta = 2 ;
+let gama = 3 ;
 let monkey = [ alpha, beta, gama ] ;
-console.log = (monkey) ;
+console.log(monkey) ;
